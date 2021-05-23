d32467f51fb1897224cef6f032e963985f96d3cd
isbora <b.isbora@gmail.com>
2021-05-23 20:39:29 +0300

    Add project files.

---
+++ WebApi/Controllers/Client.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/service")]
+    [ApiController]
+    public class Client : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetClientList()
+        {
+            Student s1 = new Student() { Id = Guid.NewGuid(), FullName = "Jack Black", Grade = 3.12M };
+            Student s2 = new Student() { Id = Guid.NewGuid(), FullName = "Jane White", Grade = 2.47M };
+
+            List<Student> products = new List<Student>();
+            products.Add(s1);
+            products.Add(s2);
+
+            return Ok(products);
+        }
+    }
+}
---
+++ WebApi/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebApi.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public decimal Grade { get; set; }
+    }
+}
