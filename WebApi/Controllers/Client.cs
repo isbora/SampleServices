fd059e30a369d7c0b7c223f5d95dad97c8450b1f
isbora <b.isbora@gmail.com>
2021-05-23 20:51:16 +0300

    test surum

--- WebApi/Controllers/Client.cs
+++ WebApi/Controllers/Client.cs
@@ -14,10 +14,12 @@ namespace WebApi.Controllers
         {
             Student s1 = new Student() { Id = Guid.NewGuid(), FullName = "Jack Black", Grade = 3.12M };
             Student s2 = new Student() { Id = Guid.NewGuid(), FullName = "Jane White", Grade = 2.47M };
+            Student s3 = new Student() { Id = Guid.NewGuid(), FullName = "Harry Orange", Grade = 1.28M };
 
             List<Student> products = new List<Student>();
             products.Add(s1);
             products.Add(s2);
+            products.Add(s3);
 
             return Ok(products);
         }
